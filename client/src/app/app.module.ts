d76e67de9032d570003a2d1d4ef5f1c59eef88af
bersling <bersling@gmail.com>
2023-03-20 17:30:50 +0100

    add navbar, logo, language selector

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LanguageSelectorComponent } from './navbar/language-selector/language-selector.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, LanguageSelectorComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ client/src/app/navbar/language-selector/language-selector.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss'],
+})
+export class LanguageSelectorComponent {}
