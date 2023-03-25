0d06d2061008c06a3193b34f141db3a9f94f0116
Daniel Niederberger <bersling@gmail.com>
2023-03-25 16:27:36 +0100

    Generated Types, Clickable Landing Page, Task Components (#8)

---
+++ client/src/app/recommender.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { UserService } from './user.service';
+import { Bit } from './bitmark.model';
+
+interface RecommendTask {
+  topic: string;
+  userId: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecommenderService {
+  readonly topics = ['FOOD_DRINKS', 'WORK', 'PRESENT_SIMPLE'];
+
+  constructor(private http: HttpClient, private userService: UserService) {}
+  recommendTask(topic: string): Observable<Bit> {
+    const user = this.userService.userId;
+    return of({
+      type: 'essay',
+      instruction: 'Welcome!',
+    } as Bit);
+    return this.http.post<Bit>(`/api/task/recommend`, {
+      topic,
+      user,
+    });
+  }
+}
