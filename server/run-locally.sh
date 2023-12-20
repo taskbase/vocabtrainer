62406f8b8ea46f59cbd21f437d03f84b1d9b03ca
Daren Sin <darensin01@gmail.com>
2023-12-20 09:47:02 +0100

    Upgraded Python version to 3.10

--- server/run-locally.sh
+++ server/run-locally.sh
@@ -14,4 +14,14 @@ docker stop "$CONTAINER_NAME" || true
 docker rm "$CONTAINER_NAME" || true
 docker build -t "$IMAGE_NAME" -f ./docker/Dockerfile .
 
-docker run -e "vocabtrainer_user=$VOCABTRAINER_USER" -e "api_key=$VOCABTRAINER_API_KEY" -e analytics_api_url="empty" -e feedback_api_url="https://dev-bitmark-api.taskbase.com" -e tenant_id=99 -e PYTHONPATH="./src" --name "$CONTAINER_NAME" -p "$PORTS" "$IMAGE_NAME" uvicorn main:app --proxy-headers --host 0.0.0.0 --port 51337 --reload || true
+docker run \
+  -e "vocabtrainer_user=$VOCABTRAINER_USER" \
+  -e "api_key=$VOCABTRAINER_API_KEY" \
+  -e analytics_api_url="empty" \
+  -e feedback_api_url="https://dev-bitmark-api.taskbase.com" \
+  -e tenant_id=99 \
+  -e PYTHONPATH="./src" \
+  --name "$CONTAINER_NAME" \
+  -p "$PORTS" \
+  "$IMAGE_NAME" \
+  uvicorn main:app --proxy-headers --host 0.0.0.0 --port 51337 --reload || true
